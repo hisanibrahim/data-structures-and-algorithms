d9586f173b8adbf038757bab86b108aa4b4516eb
Hisan Ibrahim <hisanibrahim@gmail.com>
2023-09-13 22:34:08 +0530

    UnionFind class

---
+++ src/UnionFind/UnionFind.ts
@@ -0,0 +1,10 @@
+class UnionFind {
+  private n: number;
+  constructor(n: number) {
+    this.n = n;
+  }
+  public union(p: number, q: number): void {}
+  public connected(p: number, q: number): boolean {
+    return false;
+  }
+}
