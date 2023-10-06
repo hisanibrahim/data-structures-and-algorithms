9a3c98de8f2a8d1ac8ae6411c6da4f7467781b52
Hisan Ibrahim <hisanibrahim@gmail.com>
2023-10-06 23:51:27 +0530

    QuickUnion implentation

--- src/DynamicConnectivityProblem/Solutions/QuickUnion.ts
+++ src/DynamicConnectivityProblem/Solutions/QuickUnion.ts
@@ -1,5 +1,24 @@
 import UnionFind from "../UnionFind";
 
-class QuickUnion extends UnionFind {}
+class QuickUnion extends UnionFind {
+  private root(i: number): number {
+    let j = i;
+    while (this.id[j] != j) {
+      j = this.id[j];
+    }
+    return j;
+  }
+  public connected(p: number, q: number): boolean {
+    return this.root(p) === this.root(q);
+    // If root of both are same, it means both connected
+  }
+  public union(p: number, q: number): void {
+    const rootP = this.root(p);
+    const rootQ = this.root(q);
+    this.id[rootP] = rootQ;
+    // Find root of p
+    // Set its root as root of q
+  }
+}
 
 export = QuickUnion;
